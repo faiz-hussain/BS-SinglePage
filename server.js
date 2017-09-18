278faf8815ffe7b77c23c9706bcbb9de3d2e7910
Faizal Hussain <faizalhussain@Faizals-MacBook-Pro.local>
2017-09-17 22:34:55 -0400

    Initial File Push

---
+++ server.js
@@ -0,0 +1,21 @@
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+app.use("/styles", express.static(__dirname +'/src/css/style.css'));
+app.use("/images", express.static(__dirname + '/src/img'));
+app.use("/scripts", express.static(__dirname + '/src/js'));
+
+// viewed at based directory http://localhost:8080/
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/src/index.html'));
+});
+
+// add other routes below
+// app.get('/about', function (req, res) {
+//   res.sendFile(path.join(__dirname + 'views/about.html'));
+// });
+
+app.listen(process.env.PORT || 8080);
